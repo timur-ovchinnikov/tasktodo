4ee9048cb6af212d01a19867a71f0cfc6b7e5b72
Timur Ovchinnikov <tov@PC-Mac.local>
2025-04-07 00:02:56 +0500

    create users table

--- migrations/env.py
+++ migrations/env.py
@@ -1,7 +1,6 @@
 from logging.config import fileConfig
 from sqlalchemy import engine_from_config, pool
 from alembic import context
-
 from app.database import Base
 from app import models  # Важно: чтобы Alembic видел все таблицы
 
@@ -10,7 +9,7 @@ config = context.config
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
-target_metadata = Base.metadata
+target_metadata = models.Base.metadata
 
 def run_migrations_offline():
     """Run migrations in 'offline' mode."""
--- migrations/versions/46895581fb02_create_users_table.py
+++ migrations/versions/46895581fb02_create_users_table.py
@@ -1,8 +1,8 @@
 """create users table
 
-Revision ID: 55ed5735be36
+Revision ID: 46895581fb02
 Revises: 
-Create Date: 2025-04-06 18:32:28.674444
+Create Date: 2025-04-06 18:55:16.275024
 
 """
 from typing import Sequence, Union
@@ -12,7 +12,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision: str = '55ed5735be36'
+revision: str = '46895581fb02'
 down_revision: Union[str, None] = None
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
